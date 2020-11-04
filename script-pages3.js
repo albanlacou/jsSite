65bb9e3cddb4cf8e1f7cb057b1c498255b1b84bc
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2020-11-04 12:47:50 +0100

    exo et script

--- script-pages3.js
+++ script-pages3.js
@@ -1,1 +1,21 @@
-dff
+document.getElementById("explication_1").style.display = "none";
+document.getElementById("explication_2").style.display = "none";
+
+const btDisplay=document.getElementById("bt_display");
+btDisplay.addEventListener("click",displayImage);
+
+console.log(btDisplay);
+
+function clik(){
+    
+    let name=document.getElementById("name").value;
+    document.getElementById("h2").textContent =name;
+    console.log(name);
+}
+  
+
+function displayImage(){
+    document.getElementById("explication_1").style.display = "block";
+    document.getElementById("explication_2").style.display = "block";
+}
+
