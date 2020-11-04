ac276b45ad3497e0aa85441a7a8151e1c1a8862a
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2020-11-04 15:13:47 +0100

    jeu et script

--- script-pages3.js
+++ script-pages3.js
@@ -19,3 +19,66 @@ function displayImage(){
     document.getElementById("explication_2").style.display = "block";
 }
 
+//jeu smash 
+
+let randomNumber = Math.floor(Math.random() * 6) + 1;
+      const guesses = document.querySelector('.guesses');
+      const lastResult = document.querySelector('.lastResult');
+      const lowOrHi = document.querySelector('.lowOrHi');
+      const guessSubmit = document.querySelector('.guessSubmit');
+      const guessField = document.querySelector('.guessField');
+      let guessCount = 1;
+      let resetButton;
+      guessField  
+      function checkGuess(){
+        let userGuess = Number(guessField.value);
+        if (guessCount === 1) {
+          guesses.textContent = 'vos différents éssai: ';
+        }
+        guesses.textContent += userGuess + ' ';   //envoi une réponse en fonction de la réponse du joueur
+        if (userGuess === randomNumber) {
+          lastResult.textContent = 'félicitation vous avez trouvez en ' + guessCount + " fois ! Si vous voullez recommencer cliquer sur le bouton Start New game ";
+          lastResult.style.backgroundColor = 'green';
+          lowOrHi.textContent = '';
+          setGameOver()
+         
+        } else {
+          lastResult.textContent = 'faux!';
+          lastResult.style.backgroundColor = 'red';
+          if(userGuess < randomNumber) {
+            lowOrHi.textContent = ' le dernier lancer est trop petit' ;
+          } else if(userGuess > randomNumber) {
+            lowOrHi.textContent = 'le dernier lancer est trop grand!';
+          }
+        }
+        guessCount++; //incrémente de 1
+        guessField.value = '';
+        guessField.focus();
+      }
+      guessSubmit.addEventListener('click', checkGuess); // cette fonction permet de remettre a zero le jeux et de pouvoir relancer une partie
+      function setGameOver() {
+        guessField.disabled = true;
+        guessSubmit.disabled = true;
+        resetButton = document.createElement('button');
+        resetButton.textContent = 'Start new game';
+        document.body.appendChild(resetButton);
+        resetButton.addEventListener('click', resetGame);
+      }
+     
+      function resetGame() {
+        guessCount = 1;
+        const resetParas = document.querySelectorAll('.resultParas p');
+        for(let i = 0 ; i < resetParas.length ; i++) {
+          resetParas[i].textContent = '';
+        }
+        resetButton.parentNode.removeChild(resetButton); //desactive les bouton lorsque le joueur a donnez dix réponse erroner
+        guessField.disabled = false; //desactive la zone de caractère lorsque le joueur arrive à dix tentatives erroner
+        guessSubmit.disabled = false;//desactive le bouton d'envoi lorsque le joueur arrive à dix tentatives erroner
+        guessField.value = ''; //remet la barre de texte a zero
+        guessField.focus();
+        lastResult.style.backgroundColor = 'white';
+        randomNumber = Math.floor(Math.random() * 6) + 1;
+      }
+      
+    
+
