8b00ac3cd435f8dc2db950da60f4621492bfe6c8
TortueBleu <klarir.games@gmail.com>
2020-11-05 15:41:50 +0100

    ui

--- script-pages1.js
+++ script-pages1.js
@@ -1,9 +1,46 @@
-function Champion(){
-    document.getElementById("gnar.png").display = block;
-}
-function Principe(){
+let champion = document.getElementById("Champion");
+let roles = document.getElementById("Roles");
+let principe = document.getElementById("Principe");
+let jeu = document.getElementById("Jeu");
+let text = document.getElementById("text");
 
-}
-function Roles(){
+document.getElementById("gnar").style.display = "none";
+document.getElementById("kayn").style.display = "none";
+document.getElementById("jeu").style.display = "none";
 
+
+
+champion.addEventListener("click", truc)
+roles.addEventListener("click", truc)
+principe.addEventListener("click", truc)
+jeu.addEventListener("click", truc)
+
+
+function truc(event){
+    console.log(event.target.id);
+    if (event.target.id === "Champion"){
+        document.getElementById("gnar").style.display = "block";
+        text.innerHTML= "ta maman la chauve ";
+        document.getElementById("kayn").style.display = "block";
+        document.getElementById("jeu").style.display = "none";
+
+    }
+    else if (event.target.id === "Roles"){
+        document.getElementById("gnar").style.display = "none";
+        text.innerHTML="differents roles";
+        document.getElementById("kayn").style.display = "none";
+        document.getElementById("jeu").style.display = "none";
+    }
+    else if (event.target.id === "Principe"){
+        document.getElementById("gnar").style.display = "none";
+        text.innerHTML="resumer du jeu";
+        document.getElementById("kayn").style.display = "none";
+        document.getElementById("jeu").style.display = "none";
+    }
+    else if (event.target.id === "Jeu"){
+        document.getElementById("gnar").style.display = "none";
+        text.innerHTML="cliquer pour le jeu";
+        document.getElementById("kayn").style.display = "none";
+        document.getElementById("jeu").style.display = "block";
+    }
 }
